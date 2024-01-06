3678d747123027ce0e421c804fcd709f262c5146
xulele <xulele.nlp@gmail.com>
2024-01-06 20:30:36 +0800

    fix export bug

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,6 @@
 const { i18n } = require('./next-i18next.config');
 const buildWithDocker = process.env.DOCKER === 'true';
+const isExport = process.env.NODE_ENV === 'production' && process.env.EXPORT === 'true';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   i18n,
@@ -16,5 +17,14 @@ const nextConfig = {
   // output: buildWithDocker ? 'standalone' : undefined,
   output: i18n ? undefined : 'export', 
 };
+
+if (isExport) {
+  // 在使用 next export 时禁用 i18n
+  nextConfig.i18n = {
+    locales: ['zh'], // 设置默认语言或者其他需要的语言
+    defaultLocale: 'zh',
+  };
+}
+
 
 module.exports = nextConfig;
